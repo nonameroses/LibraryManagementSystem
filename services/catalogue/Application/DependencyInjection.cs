f01dc43eed478a78e46325bc26ada3124e45450c
Adomas Garalis <U2078658@unimail.hud.ac.uk>
2023-12-29 08:34:35 +0000

    di

---
+++ services/catalogue/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+
+        //services.AddMediatR(cfg => {
+        //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        //});
+
+        return services;
+
+    }
+}
---
+++ services/catalogue/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Data.Repositories;
+using Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this WebApplicationBuilder builder)
+    {
+        var applicationAssembly = typeof(CatalogueApplication).Assembly;
+        builder.Services.Configure<MongoOptions>(builder.Configuration.GetSection("MongoDB"));
+        builder.Services.AddSingleton<MongoDbContext>();
+        builder.Services.AddTransient(typeof(IMongoRepository<,>), typeof(MongoRepository<,>));
+        builder.Services.AddTransient<IBookRepository, BookRepository>();
+
+
+        //services.AddMediatR(cfg => {
+        //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        //});
+
+        return services;
+
+    }
+    //public static void UseCatalogInfrastructure(this WebApplication app)
+    //{
+    //    app.UseInfrastructure(app.Environment);
+    //}
+}
